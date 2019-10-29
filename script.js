aab018bc309c8862ca653c2ddc9083d150567b4f
Alexandra Ackerman <aaackerman.dev@gmail.com>
2019-10-29 19:12:11 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,3 @@ document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.parallax');
   var instances = M.Parallax.init(elems);
 });
-
-document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.collapsible');
-  var instances = M.Collapsible.init(elems);
-});
