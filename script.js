38a6234123a5ab17187d98dee91bb7ea27ec8ede
Alexandra Ackerman <aaackerman.dev@gmail.com>
2019-10-29 19:10:59 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.parallax');
+  var instances = M.Parallax.init(elems);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.collapsible');
+  var instances = M.Collapsible.init(elems);
+});
